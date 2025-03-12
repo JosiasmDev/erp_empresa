28bd645c510527360b670b7fcbd9a6267c400b08
JosiasmDev <josiasdicampus@gmail.com>
2025-03-12 15:00:45 +0100

    12.03 erp_empresa

--- erp_empresa/views.py
+++ erp_empresa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from ecommerce.models import Coche
 
 def home(request):
@@ -6,3 +6,17 @@ def home(request):
     coches = Coche.objects.filter(nombre__in=['Eclipse', 'Arrow'])
     context = {'coches': coches}
     return render(request, 'home.html', context)
+
+def index(request):
+    return render(request, 'website/index.html')
+
+def contacto(request):
+    return render(request, 'website/contacto.html')
+
+def coche_list(request):
+    coches = Coche.objects.all()
+    return render(request, 'website/coche_list.html', {'coches': coches})
+
+def coche_detalle(request, coche_id):
+    coche = get_object_or_404(Coche, pk=coche_id)
+    return render(request, 'website/coche_detalle.html', {'coche': coche})
